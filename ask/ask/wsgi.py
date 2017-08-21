97a32320d63570b6a9be2355980c2fe9f93f0b0a
Vasiliy Lifanovskiy <vasiliy.lifanovskiy@ddgcorp.ru>
2017-08-21 19:04:57 +0300

    add sys append path

--- ask/ask/wsgi.py
+++ ask/ask/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/home/box/web/ask')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ask.settings")
 
 application = get_wsgi_application()
