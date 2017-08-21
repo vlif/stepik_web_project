895757a3d2f4f2cf6c80a7e7696ae90d2bd53052
Vasiliy Lifanovskiy <vasiliy.lifanovskiy@ddgcorp.ru>
2017-08-21 18:51:48 +0300

    added view

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# В файле ask/qa/views.py создайте тестовый контроллер
+def test(request, *args, **kwargs):
+    return HttpResponse('OK')
