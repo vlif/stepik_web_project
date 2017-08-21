f18a056e4f9cdf2a0168b3d7acb947185f7e2741
Vasiliy Lifanovskiy <vasiliy.lifanovskiy@ddgcorp.ru>
2017-08-21 19:16:06 +0300

    add ask ask urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,4 +18,12 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^$', include('qa.urls')),
+    url(r'^login/', include('qa.urls')),
+    url(r'^signup/', include('qa.urls')),
+    url(r'^ask/', include('qa.urls')),
+    url(r'^popular/', include('qa.urls')),
+    url(r'^new/', include('qa.urls')),
+    url(r'^question/', include('qa.urls')),
 ]
