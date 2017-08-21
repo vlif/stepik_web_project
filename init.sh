b1052a2c06830d52a81461a97b150c1c59eee3ca
Vasiliy Lifanovskiy <vasiliy.lifanovskiy@ddgcorp.ru>
2017-08-21 18:03:39 +0300

    updated init.sh

--- init.sh
+++ init.sh
@@ -2,11 +2,13 @@ sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-sudo pip install --upgrade django
-sudo pip install --upgrade gunicorn
+#sudo pip install --upgrade django
+#sudo pip install --upgrade gunicorn
 
-django-admin startproject ask
-./ask/manage.py startapp qa
+sudo gunicorn - b 0.0.0.0:8080 hello:app &
+
+#django-admin startproject ask # создать проект
+#python ./ask/manage.py startapp qa # создать приложение
 
 #sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
