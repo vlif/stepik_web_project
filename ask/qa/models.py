21023e43f95c4400144838fb30d4b4633329b169
Vasiliy Lifanovskiy <vasiliy.lifanovskiy@ddgcorp.ru>
2017-09-14 16:11:25 +0300

    added questionmanager

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,7 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class QuestionManager(models.Manager):
+	def new(self):
+		return self.order_by('-added_at')
+	def popular(self):
+		return self.order_by('-rating')
+
 class Question(models.Model):
+	objects = QuestionManager()
+
 	title = models.CharField(max_length=255)
 	text = models.TextField()
 	added_at = models.DateTimeField(blank=True, auto_now_add=True)
