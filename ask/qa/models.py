6c04d2999bec3f023ef8ede46952ecf51f0ac82e
Vasiliy Lifanovskiy <vasiliy.lifanovskiy@ddgcorp.ru>
2017-09-14 16:02:26 +0300

    changed models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -7,11 +7,11 @@ class Question(models.Model):
 	text = models.TextField()
 	added_at = models.DateTimeField(blank=True, auto_now_add=True)
 	rating = models.IntegerField(default=0)
-	author = models.ForeignKey(User, on_delete=models.SET_NULL)
+	author = models.ForeignKey(User) #, on_delete=models.SET_NULL
 	likes = models.ManyToManyField(User, related_name='question_like_user')
 
 class Answer(models.Model):
 	text = models.TextField()
 	added_at = models.DateTimeField(blank=True, auto_now_add=True)
-	question = models.ForeignKey(Question, on_delete=models.CASCADE)
-	author = models.ForeignKey(User, on_delete=models.SET_NULL)
+	question = models.ForeignKey(Question) #, on_delete=models.CASCADE
+	author = models.ForeignKey(User) #, on_delete=models.SET_NULL
